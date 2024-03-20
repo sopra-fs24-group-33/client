c055d1d3bda899c0387a8943b2a8b4a719b69a6f
Miguel <miguel-meierbluewin.ch>
2024-03-20 10:38:00 +0100

    m2

--- src/components/views/Login.tsx
+++ src/components/views/Login.tsx
@@ -38,53 +38,53 @@ const Login = () => {
   const [name, setName] = useState<string>(null);
   const [username, setUsername] = useState<string>(null);
 
-  const doLogin = async () => {
-    try {
-      const requestBody = JSON.stringify({ username, name });
-      const response = await api.post("/users", requestBody);
+  // const doLogin = async () => {
+  //   try {
+  //     const requestBody = JSON.stringify({ username, name });
+  //     const response = await api.post("/users", requestBody);
+  //
+  //     // Get the returned user and update a new object.
+  //     const user = new User(response.data);
+  //
+  //     // Store the token into the local storage.
+  //     localStorage.setItem("token", user.token);
+  //
+  //     // Login successfully worked --> navigate to the route /game in the GameRouter
+  //     navigate("/game");
+  //   } catch (error) {
+  //     alert(
+  //       `Something went wrong during the login: \n${handleError(error)}`
+  //     );
+  //   }
+  // };
 
-      // Get the returned user and update a new object.
-      const user = new User(response.data);
-
-      // Store the token into the local storage.
-      localStorage.setItem("token", user.token);
-
-      // Login successfully worked --> navigate to the route /game in the GameRouter
-      navigate("/game");
-    } catch (error) {
-      alert(
-        `Something went wrong during the login: \n${handleError(error)}`
-      );
-    }
-  };
-
-  return (
-    <BaseContainer>
-      <div className="login container">
-        <div className="login form">
-          <FormField
-            label="Username"
-            value={username}
-            onChange={(un: string) => setUsername(un)}
-          />
-          <FormField
-            label="Name"
-            value={name}
-            onChange={(n) => setName(n)}
-          />
-          <div className="login button-container">
-            <Button
-              disabled={!username || !name}
-              width="100%"
-              onClick={() => doLogin()}
-            >
-              Login
-            </Button>
-          </div>
-        </div>
-      </div>
-    </BaseContainer>
-  );
+  // return (
+  //   <BaseContainer>
+  //     <div className="login container">
+  //       <div className="login form">
+  //         <FormField
+  //           label="Username"
+  //           value={username}
+  //           onChange={(un: string) => setUsername(un)}
+  //         />
+  //         <FormField
+  //           label="Name"
+  //           value={name}
+  //           onChange={(n) => setName(n)}
+  //         />
+  //         <div className="login button-container">
+  //           <Button
+  //             disabled={!username || !name}
+  //             width="100%"
+  //             onClick={() => doLogin()}
+  //           >
+  //             Login
+  //           </Button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   </BaseContainer>
+  // );
 };
 
 /**
